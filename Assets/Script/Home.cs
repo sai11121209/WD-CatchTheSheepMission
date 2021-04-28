bfd73ccd675f79c8034c56d08713c39703bf2ec9
sai11121209 <sai11121209@gmail.com>
2021-04-28 17:31:27 +0900

    #02 バグ修正

---
+++ Assets/Script/Home.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Home : MonoBehaviour
+{
+    public Text pointText;
+    public Text timeText;
+    private int point = 0;
+    private float countTime = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointText.text = "Point: " + point.ToString();
+        timeText.text = "Time: " + countTime.ToString("F2");
+        if (point < 100)
+        {
+            countTime += Time.deltaTime;
+        }
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Sheep")
+        {
+            Destroy(collision.gameObject);
+            point++;
+        }
+        if (point == 100)
+        {
+            //SceneManager.LoadScene("ResultScene");
+        }
+    }
+}
---
+++ Assets/Script/SheepController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SheepController : MonoBehaviour
+{
+    public GameObject sheep;
+    private float x, y, z;
+
+    private int sheepCount = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 100; i++)
+        {
+            x = Random.Range(-18.0f, 18.0f);
+            y = Random.Range(0.5f, 19.0f);
+            z = Random.Range(-18.0f, 18.0f);
+            x = 0;
+            y = 2;
+            z = 22;
+            Instantiate(sheep.gameObject, new Vector3(x, y, z), Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
